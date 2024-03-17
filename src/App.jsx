9c8c7df876270e5aeab6cdd918b22955d44bb05d
shamehaera9 <shamehaera9@gmail.com>
2024-03-17 13:01:05 +0600

    toast added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import Menus from './Componants/Menus/Menus'
 import Nav from './Componants/Nav_bar/Nav'
 import Our_products from './Componants/Our_products/Our_products'
 import Want_cook from './Componants/Want_cook/Want_cook'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify'
 
 function App() {
   const[wanttocook,setWantCook]=useState([]);
   const wantCook = (menu) =>{
     const isExist = wanttocook.find((pd) => pd.recipe_id == menu.recipe_id);
     if(!isExist){
+      
    setWantCook([...wanttocook,menu]);
+    }
+    else{
+      toast("Added Already");
     }
   }
 
@@ -34,7 +40,9 @@ function App() {
 
      <Want_cook wanttocook={wanttocook} ></Want_cook>
       </div>
+      <ToastContainer/>
      </div>
+     
     </>
   )
 }
